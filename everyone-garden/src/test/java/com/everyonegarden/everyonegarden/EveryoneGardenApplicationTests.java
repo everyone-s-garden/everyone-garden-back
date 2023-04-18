ecc7970b435f20debab811b442f73b2ae3766f6b
jinkyumpark <jinpark1025@gmail.com>
2023-04-18 22:06:07 +0900

    initial commit

---
+++ everyone-garden/src/test/java/com/everyonegarden/everyonegarden/EveryoneGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everyonegarden.everyonegarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryoneGardenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
