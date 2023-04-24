ea379b5c96fbfb9836d4f54bcc2d2173890c817b
byeolhaha <j35635aa@gmail.com>
2023-04-24 16:21:59 +0900

    SecurityConfig 추가

--- everyone-garden/src/main/java/com/everyonegarden/auth/config/SecurityConfig.java
+++ everyone-garden/src/main/java/com/everyonegarden/auth/config/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.everyonegarden.auth.config;
 
+import com.everyonegarden.auth.config.jwt.JwtAuthenticationFilter;
+import com.everyonegarden.auth.config.jwt.JwtAuthorizationFilter;
 import com.everyonegarden.user.repository.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +24,48 @@ public class SecurityConfig {
     private UserRepository userRepository;
 
     @Autowired
-    private Cor
+    private CorsConfig corsConfig ;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http
+                .addFilter(corsConfig.corsFilter())
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .addFilter(new JwtAuthenticationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))))
+                .addFilter(new JwtAuthorizationFilter(
+                        authenticationManager(http.getSharedObject(AuthenticationConfiguration.class))
+                        , userRepository))
+                .authorizeRequests()
+                .antMatchers("/post/**")
+                .access("hasRole('ROLE_USER')")
+                .anyRequest().permitAll();
+
+
+
+
+        return http.build();
+
+
+
+    }
+
+
 
 
 }
