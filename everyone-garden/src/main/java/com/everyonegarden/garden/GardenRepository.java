15464cb5a314ff67134133a5449c432c1fcfccfe
jinkyumpark <jinpark1025@gmail.com>
2023-04-25 15:11:31 +0900

    add garden response, repository

--- everyone-garden/src/main/java/com/everyonegarden/garden/GardenRepository.java
+++ everyone-garden/src/main/java/com/everyonegarden/garden/GardenRepository.java
@@ -1,7 +1,10 @@
 package com.everyonegarden.garden;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GardenRepository extends JpaRepository<Garden, Long> {}
--- everyone-garden/src/main/java/com/everyonegarden/garden/response/GardenResponse.java
+++ everyone-garden/src/main/java/com/everyonegarden/garden/response/GardenResponse.java
@@ -1,5 +1,6 @@
 package com.everyonegarden.garden.response;
 
+import com.everyonegarden.garden.GardenType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,4 +9,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor @AllArgsConstructor @Builder
 public class GardenResponse {
+    private Long id;
+
+    private String name;
+    private GardenType type;
+
+    private String address;
+    private String longitude;
+    private String latitude;
+
+    private String link;
+    private Integer price;
 }
