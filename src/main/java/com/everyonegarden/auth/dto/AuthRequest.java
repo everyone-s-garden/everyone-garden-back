421b82550d741a324d6e90a968db84dd135a27b5
byeolhaha <j35635aa@gmail.com>
2023-04-25 17:55:33 +0900

    프론트로부터 받은 accessToken dto

---
+++ src/main/java/com/everyonegarden/auth/dto/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.everyonegarden.auth.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String accessToken;
+}
