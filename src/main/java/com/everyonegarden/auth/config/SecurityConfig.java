cc179efa701ff0b9b63ae99e72fc21a1b2f863d2
byeolhaha <j35635aa@gmail.com>
2023-04-25 17:42:49 +0900

    JWT 토큰 생성 및 검증

---
+++ src/main/java/com/everyonegarden/auth/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.everyonegarden.auth.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthToken
+
+
+}
