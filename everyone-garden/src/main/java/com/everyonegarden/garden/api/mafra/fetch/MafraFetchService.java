484b674809b5530468dc055a6b6e4db76c3a1524
jinkyumpark <jinpark1025@gmail.com>
2023-05-02 02:31:11 +0900

    modify mafra api

--- everyone-garden/src/main/java/com/everyonegarden/garden/api/mafra/fetch/MafraFetchService.java
+++ everyone-garden/src/main/java/com/everyonegarden/garden/api/mafra/fetch/MafraFetchService.java
@@ -23,18 +23,32 @@ public class MafraFetchService {
 
     // start, end -> 1부터 API에 있는 모든 정보 개수까지 (0부터 시작하지 않음에 주의)
     // 예) start = 5, end = 20 -> 등록 날짜 DESC 순서대로 5번째 부터 20번째까지 15개
-    public List<ApiMafraRow> getMafraApiResponse(int start, int end) {
+    public List<ApiMafraRow> getMafraRows(int start, int end) {
         String url = REQUEST_URI + "?" +
                 "API_KEY=" + API_KEY +
                 "&TYPE=" + "JSON" +
                 "&API_URL=" + "Grid_20171122000000000552_1" +
                 "&START_INDEX=" + start +
                 "&END_INDEX=" + end;
+
         ApiMafraResponse response = restTemplate.getForObject(url, ApiMafraResponse.class);
 
         if (response == null || response.getRow() == null) return List.of();
 
         return response.getRow();
+    }
+
+    // start, end -> 1부터 API에 있는 모든 정보 개수까지 (0부터 시작하지 않음에 주의)
+    // 예) start = 5, end = 20 -> 등록 날짜 DESC 순서대로 5번째 부터 20번째까지 15개
+    public ApiMafraResponse getMafraApiResponse(int start, int end) {
+        String url = REQUEST_URI + "?" +
+                "API_KEY=" + API_KEY +
+                "&TYPE=" + "JSON" +
+                "&API_URL=" + "Grid_20171122000000000552_1" +
+                "&START_INDEX=" + start +
+                "&END_INDEX=" + end;
+
+        return restTemplate.getForObject(url, ApiMafraResponse.class);
     }
 
 }
