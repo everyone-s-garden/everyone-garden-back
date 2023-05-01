7ff81a6a2f224cf908aa646f7b4d37be6a665ea1
jinkyumpark <jinpark1025@gmail.com>
2023-05-02 02:32:29 +0900

    add main endpoint for api

---
+++ everyone-garden/src/main/java/com/everyonegarden/main/MainResponse.java
@@ -0,0 +1,22 @@
+package com.everyonegarden.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class MainResponse {
+
+
+    @DateTimeFormat(pattern = "yyyy-MM-DDThh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private Integer status;
+    private String message;
+
+}
