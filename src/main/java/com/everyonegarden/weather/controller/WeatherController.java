0d8517460fc36266bcd69b9e1dff36c5cf20f196
byeolhaha <j35635aa@gmail.com>
2023-05-23 20:18:35 +0900

    fix : WeatherConroller API 수정

--- src/main/java/com/everyonegarden/weather/controller/WeatherController.java
+++ src/main/java/com/everyonegarden/weather/controller/WeatherController.java
@@ -10,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("v1/weather")
 public class WeatherController {
 
     public final WeatherResponseService weatherResponseService;
@@ -25,7 +27,7 @@ public class WeatherController {
     /*
      * 위치를 지정한 경우
      */
-    @GetMapping("/weather")
+    @GetMapping("/time")
     public ResponseEntity<ApiWeatherResult> weatherRegionRequest( @RequestParam("lat") String lat,
                                                                   @RequestParam("long") String lng) throws Exception {
         return weatherOneRegionService.getRegionWeather(lat,lng);
@@ -35,7 +37,7 @@ public class WeatherController {
     /*
      * 위치를 지정하지 않은 경우
      */
-    @GetMapping("/weather/all")
+    @GetMapping("/all")
     public ResponseEntity<ApiWeatherResult> weatherAllRequest() throws Exception {
 
         return weatherAllRegionService.getAllRegionWeather();
