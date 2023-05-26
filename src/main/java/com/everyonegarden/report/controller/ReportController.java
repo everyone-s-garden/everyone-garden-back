6c3e42ce9ea044b4888b84442e50351176ad072d
byeolhaha <j35635aa@gmail.com>
2023-05-26 21:02:15 +0900

    add : 게시글 신고 기능 틀 생성

---
+++ src/main/java/com/everyonegarden/report/controller/ReportController.java
@@ -0,0 +1,21 @@
+package com.everyonegarden.report.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/report")
+public class ReportController {
+
+
+
+
+
+
+
+
+}
