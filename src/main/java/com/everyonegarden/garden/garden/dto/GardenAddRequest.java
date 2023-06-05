6f8d1e0663bcf048bf30f2d34cceb44f924f57be
jinkyumpark <jinpark1025@gmail.com>
2023-06-05 22:58:38 +0900

    Garden refactoring, 수정사항 반영
    - 내 텃밭 등록하기 useStartDate, useEndDate (`.`으로 구분)
    - 날짜 Response는 다 `.`으로 구분하기
    - 시설 상태 정보 (화장실, 수로, 농기구)
    - 텃밭 매물 등록할 때 type이 null임 → 자동으로 PRIVATE으로 되야 함
    - GardenDetail → `facility` 추가

---
+++ src/main/java/com/everyonegarden/garden/garden/dto/GardenAddRequest.java
@@ -0,0 +1,84 @@
+package com.everyonegarden.garden.garden.dto;
+
+import com.everyonegarden.garden.garden.Garden;
+import com.everyonegarden.garden.garden.GardenStatus;
+import com.everyonegarden.garden.garden.GardenType;
+import com.everyonegarden.garden.gardenImage.GardenImage;
+import com.everyonegarden.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class GardenAddRequest {
+
+    private List<String> images;
+
+    @NotBlank(message = "name은 null이거나 빈칸 (\"\")일 수 없어요")
+    private String name;
+    private String price;
+    private String size;
+    private String contact;
+    private String status;
+    private String content;
+
+    private String address;
+    private Double latitude;
+    private Double longitude;
+
+    private GardenAddRequestFacility facility;
+
+    private String link;
+
+    public Garden toEntity(Long memberId) {
+        return Garden.builder()
+                .member(Member.builder().id(memberId).build())
+
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+
+                .name(name)
+
+                .type(GardenType.MANUAL)
+                .status(GardenStatus.valueOf(status.toUpperCase()))
+
+                .link(link)
+                .price(price)
+
+                .content(content)
+                .size(size)
+
+                .content(content)
+
+                .toilet(facility.getToilet())
+                .waterway(facility.getWaterway())
+                .equipment(facility.getEquipment())
+
+                .build();
+    }
+
+    public List<GardenImage> getImages(Long gardenId) {
+        return images.stream()
+                .map(image -> GardenImage.builder()
+                        .url(image)
+                        .garden(Garden.builder().gardenId(gardenId).build())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+}
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+class GardenAddRequestFacility {
+    private Boolean toilet;
+    private Boolean waterway;
+    private Boolean equipment;
+}
---
+++ src/main/java/com/everyonegarden/garden/garden/dto/GardenDetailResponse.java
@@ -0,0 +1,88 @@
+package com.everyonegarden.garden.garden.dto;
+
+import com.everyonegarden.garden.garden.Garden;
+import com.everyonegarden.garden.gardenImage.GardenImage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class GardenDetailResponse {
+
+    private Long gardenId;
+
+    private String address;
+    private double latitude;
+    private double longitude;
+
+    private String name;
+    private String type;
+    private String link;
+    private Integer price;
+
+    private String contact;
+    private String size;
+
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate recruitStartDate;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate recruitEndDate;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate useStartDate;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDate useEndDate;
+
+    private String postTitle;
+    private String postContent;
+    private List<String> images;
+
+    private GardenDetailResponseFacility facility;
+
+    public static GardenDetailResponse of(Garden garden) {
+        return GardenDetailResponse.builder()
+                .gardenId(garden.getGardenId())
+
+                .address(garden.getAddress())
+                .latitude(garden.getLatitude())
+                .longitude(garden.getLongitude())
+
+                .name(garden.getName())
+                .type(garden.getType().toString())
+                .link(garden.getLink())
+                .price(Integer.valueOf(garden.getPrice()))
+                .contact(garden.getContact())
+                .size(garden.getSize())
+
+                .recruitStartDate(LocalDate.from(garden.getRecruitStartDate()))
+                .recruitEndDate(LocalDate.from(garden.getRecruitEndDate()))
+                .useStartDate(LocalDate.from(garden.getUseStartDate()))
+                .useEndDate(LocalDate.from(garden.getUseEndDate()))
+
+                .images(garden.getImages().stream().map(GardenImage::getUrl).collect(Collectors.toList()))
+
+                .facility(GardenDetailResponseFacility.builder()
+                        .toilet(garden.getToilet() != null && garden.getToilet())
+                        .waterway(garden.getWaterway() != null && garden.getWaterway())
+                        .equipment(garden.getEquipment() != null && garden.getEquipment())
+                        .build()
+                )
+
+                .build();
+    }
+
+}
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
+class GardenDetailResponseFacility {
+    private boolean toilet;
+    private boolean waterway;
+    private boolean equipment;
+}
