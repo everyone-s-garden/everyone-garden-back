a73c09f54d0a86b16cd20dbe6de44496440653bd
jinkyumpark <jinpark1025@gmail.com>
2023-06-05 22:58:57 +0900

    exception handling 추가

---
+++ src/main/java/com/everyonegarden/common/exception/ExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.everyonegarden.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleValidationExceptions(Exception ex) {
+        BindingResult bindingResult;
+        if (ex instanceof MethodArgumentNotValidException) {
+            bindingResult = ((MethodArgumentNotValidException) ex).getBindingResult();
+        } else {
+            bindingResult = ((BindException) ex).getBindingResult();
+        }
+
+        StringBuilder errorMessage = new StringBuilder();
+        bindingResult.getAllErrors().forEach(error -> errorMessage.append(error.getDefaultMessage()).append("\n"));
+        return ResponseEntity.badRequest().body(errorMessage.toString());
+    }
+
+}
