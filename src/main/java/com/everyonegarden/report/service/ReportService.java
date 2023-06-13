048ac82e0d7aa36a2091650f232361495852d5d7
byeolhaha <j35635aa@gmail.com>
2023-06-13 19:47:29 +0900

    fix : 중복된 접수의 경우 작성자에 대한 정보에서 게시글에 대한 정보를 찾도록 로직 수정

--- src/main/java/com/everyonegarden/report/service/ReportService.java
+++ src/main/java/com/everyonegarden/report/service/ReportService.java
@@ -1,8 +1,8 @@
 package com.everyonegarden.report.service;
 
 
+import com.everyonegarden.garden.garden.Garden;
 import com.everyonegarden.garden.garden.GardenRepository;
-import com.everyonegarden.member.entity.Member;
 import com.everyonegarden.report.dto.ReportRequestDto;
 import com.everyonegarden.report.entity.Report;
 import com.everyonegarden.report.repository.ReportRepository;
@@ -18,10 +18,11 @@ public class ReportService {
 
     private final ReportRepository reportRepository;
     private final GardenRepository gardenRepository;
-    private final WriterScoreService writerScoreService;
+    private final PostScoreService writerScoreService;
 
-    public String registerReport(Long postId, Long reporterId, ReportRequestDto reportRequestDto){
+    public String registerReport(Long reporterId, ReportRequestDto reportRequestDto){
 
+        Long postId = reportRequestDto.getPostId();
         Optional<Report> report = reportRepository.findByPostIdAndReporterId(postId,reporterId);
 
         //게시글에 대한 신고가 없는 경우
@@ -32,11 +33,9 @@ public class ReportService {
                     .build();
             //저장
             reportRepository.save(newReport);
-            Member writer = gardenRepository.findByGardenId(postId).getMember();
-
-            String writerId = writer.getSocialId();
+            Garden garden = gardenRepository.findByGardenId(postId);
             // 작성자 찾아서 점수 누적하고 영구정지 여부 만들기
-            writerScoreService.sumReportScore(newReport,writerId,reportRequestDto);
+            writerScoreService.sumReportScore(newReport,garden,reportRequestDto);
 
             return "신고가 완료되었습니다.";
         }
