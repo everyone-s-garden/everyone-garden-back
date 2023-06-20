f74b3ddda975f986e0c6237ddc7a5695cee580c6
byeolhaha <j35635aa@gmail.com>
2023-06-20 21:02:23 +0900

    feat : 자동 지역명 검색 API controller 구현

---
+++ src/main/java/com/everyonegarden/location/controller/LocationController.java
@@ -0,0 +1,35 @@
+package com.everyonegarden.location.controller;
+
+
+import com.everyonegarden.common.dto.ApiResponse;
+import com.everyonegarden.location.dto.LocationResponse;
+import com.everyonegarden.location.service.AutoCompleteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class LocationController {
+
+    private final AutoCompleteService autoCompleteService;
+
+    private final Pageable pageable = PageRequest.of(0, 5);
+
+    @GetMapping(value = "/v1/location")
+    public List<LocationResponse> LocationRequest(@RequestParam String address) {
+
+        String[] tokenArr = autoCompleteService.getAddressLevel(address);
+
+        return  autoCompleteService.autoCompleteLocation(tokenArr,pageable);
+    }
+}
