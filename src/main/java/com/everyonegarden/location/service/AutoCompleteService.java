3242fcba8a773cdd5b264dfbad3b2bef9113b380
byeolhaha <j35635aa@gmail.com>
2023-06-22 00:27:19 +0900

    fix : Query 수정에 따라 Stream을 적용하여 Service 메서드 수정

--- src/main/java/com/everyonegarden/location/service/AutoCompleteService.java
+++ src/main/java/com/everyonegarden/location/service/AutoCompleteService.java
@@ -13,47 +13,16 @@ import org.springframework.stereotype.Service;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class AutoCompleteService {
 
     private final LocationRepository locationRepository;
-    public List<LocationResponse> autoCompleteLocation(List<Location> locationList){
-        List<LocationResponse> autoLocationList = new ArrayList<>();
-
-        for(Location location : locationList){
-            autoLocationList.add(new LocationResponse(location.getFullAddress(),location.getLatitude(), location.getLongitude()));
-        }
-
-        return autoLocationList;
-    }
-
-    public String[] getAddressLevelArr(String address){
-        String[] levelsArr = new String[4];
-        String[] tokenArr = address.split(" ");
-
-        for(int i=0;i< tokenArr.length;i++){
-            levelsArr[i]=tokenArr[i];
-        }
-        return levelsArr;
-    }
-
-    public int getAddressLevel(String address){
-        String[] tokenArr = address.split(" ");
-
-        return tokenArr.length;
-    }
-
-    public List<Location> selectQuery(int level, String[] tokenArr, Pageable pageable){
-        switch (level){
-            case 1:
-                 return locationRepository.findAllLocationByLevel1(tokenArr[0], tokenArr[1], tokenArr[2], tokenArr[3],pageable);
-            case 2:
-                 return locationRepository.findAllLocationByLevel2(tokenArr[0], tokenArr[1], tokenArr[2], tokenArr[3],pageable);
-            case 3:
-                return locationRepository.findAllLocationByLevel3(tokenArr[0], tokenArr[1], tokenArr[2], tokenArr[3],pageable);
-            default:
-                return locationRepository.findAllLocationByLevel4(tokenArr[0], tokenArr[1], tokenArr[2], tokenArr[3],pageable);
-        }
+    public List<LocationResponse> autoCompleteLocation(String fullAddress, Pageable pageable){
+        return locationRepository.findAllLocation(fullAddress,pageable).stream()
+                .map(LocationResponse::buildLocationResponse)
+                .collect(Collectors.toList());
     }
 }
