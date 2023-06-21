3d15aee213c254aa4104a4056f8affd9d9760e05
byeolhaha <j35635aa@gmail.com>
2023-06-22 00:25:42 +0900

    fix : 지역 DB 변화에 따른 Query 수정

--- src/main/java/com/everyonegarden/location/dto/LocationResponse.java
+++ src/main/java/com/everyonegarden/location/dto/LocationResponse.java
@@ -1,9 +1,16 @@
 package com.everyonegarden.location.dto;
 
 
+import com.everyonegarden.garden.garden.Garden;
+import com.everyonegarden.garden.garden.dto.GardenResponse;
+import com.everyonegarden.location.Location;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 public class LocationResponse {
 
     private String position;
@@ -14,5 +21,13 @@ public class LocationResponse {
         this.position = position;
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    public static LocationResponse buildLocationResponse(Location location){
+        return LocationResponse.builder()
+                .position(location.assembleFullAddress())
+                .latitude(location.getLatitude())
+                .longitude(location.getLongitude())
+                .build();
     }
 }
