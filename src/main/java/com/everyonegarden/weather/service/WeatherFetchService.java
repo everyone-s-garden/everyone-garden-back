cab1d53ff04694345924341e3e9b1b8973c4b694
byeolhaha <j35635aa@gmail.com>
2023-06-27 11:40:06 +0900

    fix : 날짜 NPE 버그 잡기 위해 출력문 추가

--- src/main/java/com/everyonegarden/weather/service/WeatherFetchService.java
+++ src/main/java/com/everyonegarden/weather/service/WeatherFetchService.java
@@ -1,39 +1,25 @@
 package com.everyonegarden.weather.service;
 
-
-
-import com.everyonegarden.weather.service.WeatherResponseService;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonArray;
 
 import lombok.RequiredArgsConstructor;
-
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
 import java.net.URI;
-
-import java.net.URLEncoder;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
-
 
 //단기예보
 @Service
 @RequiredArgsConstructor
 public class WeatherFetchService {
-
-
     public JsonArray fetchWeather(String makeUrl) throws Exception {
-
-
+        System.out.println("makeUrl="+makeUrl);
         URI uri = new URI(makeUrl);
         RestTemplate restTemplate = new RestTemplate();
         String jsonString = restTemplate.getForObject(uri,String.class);
@@ -43,12 +29,8 @@ public class WeatherFetchService {
         JsonObject jsonBody = (JsonObject) jsonResponse.get("body");
         JsonObject jsonItems = (JsonObject) jsonBody.get("items");
 
-
         // itmes는 JSON -> 배열로 가져오기
         JsonArray jsonItemList = (JsonArray) jsonItems.get("item");
-
-        //System.out.println(urlBuilder);
-
         return jsonItemList;
     }
 
