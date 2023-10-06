56d1f78d55586b5262b35a32c7208c808e9beb95
byeolhaha <j35635aa@gmail.com>
2023-10-07 02:01:16 +0900

    [Feat] : rest docs 적용

--- src/test/java/com/everyonegarden/weather/controller/WeatherControllerTest.java
+++ src/test/java/com/everyonegarden/weather/controller/WeatherControllerTest.java
@@ -2,14 +2,14 @@ package com.everyonegarden.weather.controller;
 
 import com.everyonegarden.Fixtures;
 import com.everyonegarden.weather.facade.WeatherFacade;
-import com.everyonegarden.weather.infra.dto.WeatherApiResult;
-import com.everyonegarden.weather.infra.dto.WeatherMidResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
@@ -25,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureRestDocs
 @WebMvcTest(WeatherController.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(RestDocumentationExtension.class)
 public class WeatherControllerTest {
 
     @Autowired
@@ -47,17 +48,17 @@ public class WeatherControllerTest {
                 .andDo(document("weather-region-request",
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
                         PayloadDocumentation.responseFields(
-                                PayloadDocumentation.fieldWithPath("[].baseDate").description("Base date")
+                                PayloadDocumentation.fieldWithPath("[].baseDate").description("예측날짜")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].category").description("Category")
+                                PayloadDocumentation.fieldWithPath("[].category").description("종류")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].fcstDate").description("Forecast date")
+                                PayloadDocumentation.fieldWithPath("[].fcstDate").description("예보날짜")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].fcstTime").description("Forecast time")
+                                PayloadDocumentation.fieldWithPath("[].fcstTime").description("예보시간")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].fcstValue").description("Forecast value")
+                                PayloadDocumentation.fieldWithPath("[].fcstValue").description("예측값")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].regionName").description("Region name")
+                                PayloadDocumentation.fieldWithPath("[].regionName").description("지역 이름")
                                         .type(JsonFieldType.STRING)
                         )));
     }
@@ -76,19 +77,19 @@ public class WeatherControllerTest {
                 .andDo(document("weather-week-request",
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
                         PayloadDocumentation.responseFields(
-                                PayloadDocumentation.fieldWithPath("[].wf1Pm").description("wf1Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf1Pm").description("1일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf2Pm").description("wf2Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf2Pm").description("2일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf3Pm").description("wf3Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf3Pm").description("3일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf4Pm").description("wf4Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf4Pm").description("4일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf5Pm").description("wf5Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf5Pm").description("5일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf6Pm").description("wf6Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf6Pm").description("6일차 오후")
                                         .type(JsonFieldType.STRING),
-                                PayloadDocumentation.fieldWithPath("[].wf7Pm").description("wf7Pm description")
+                                PayloadDocumentation.fieldWithPath("[].wf7Pm").description("7일차 오후")
                                         .type(JsonFieldType.STRING),
                                 PayloadDocumentation.fieldWithPath("[].regionName").description("Region name")
                                         .type(JsonFieldType.STRING)
@@ -113,6 +114,17 @@ public class WeatherControllerTest {
                                         .type(JsonFieldType.NUMBER),
                                 PayloadDocumentation.fieldWithPath("data").description("Data object")
                                         .type(JsonFieldType.OBJECT)
+                                        .optional(),  // data 필드가 없는 경우를 허용
+                                PayloadDocumentation.fieldWithPath("data.강원").description("강원 지역의 날씨 정보")
+                                        .type(JsonFieldType.ARRAY).optional(),  // "강원" 필드가 없는 경우를 허용
+                                PayloadDocumentation.fieldWithPath("data.강원[].baseDate")
+                                        .description("날씨 기준 날짜").type(JsonFieldType.STRING),
+                                PayloadDocumentation.fieldWithPath("data.강원[].category")
+                                        .description("날씨 카테고리").type(JsonFieldType.STRING),
+                                PayloadDocumentation.fieldWithPath("data.강원[].obsrValue")
+                                        .description("관측 값").type(JsonFieldType.STRING),
+                                PayloadDocumentation.fieldWithPath("data.강원[].regionName")
+                                        .description("지역 이름").type(JsonFieldType.STRING)
                         )));
     }
 }
