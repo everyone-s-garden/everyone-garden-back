5cb2f4f878f85c2fe12e8cd02c9fa589e5b8f98f
byeolhaha <j35635aa@gmail.com>
2023-10-06 14:49:37 +0900

    [Refactor] : HTTP 헤더 필드 추가

--- src/main/java/com/everyonegarden/weather/controller/WeatherController.java
+++ src/main/java/com/everyonegarden/weather/controller/WeatherController.java
@@ -1,61 +1,64 @@
 package com.everyonegarden.weather.controller;
 
-
-import com.everyonegarden.weather.dto.ApiWeatherResult;
-import com.everyonegarden.weather.dto.ApiWeatherTimeDto;
-import com.everyonegarden.weather.dto.WeatherDto;
-import com.everyonegarden.weather.service.midterm.WeatherWeekService;
-import com.everyonegarden.weather.service.shortterm.all.WeatherAllRegionService;
-import com.everyonegarden.weather.service.WeatherResponseService;
-import com.everyonegarden.weather.service.shortterm.time.WeatherOneRegionService;
+import com.everyonegarden.weather.infra.dto.WeatherApiResult;
+import com.everyonegarden.weather.infra.dto.WeatherTimeApiResponse;
+import com.everyonegarden.weather.infra.dto.WeatherMidResponse;
+import com.everyonegarden.weather.facade.WeatherFacade;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("v1/weather")
 public class WeatherController {
 
-    public final WeatherResponseService weatherResponseService;
-    public final WeatherAllRegionService weatherAllRegionService;
-    public final WeatherOneRegionService weatherOneRegionService;
 
-    public final WeatherWeekService weatherWeekService ;
+    private final WeatherFacade weatherFacade;
 
-    /*
-     * 위치를 지정한 경우 시간대별
-     */
-    @GetMapping("/time")
-    public ResponseEntity<List<ApiWeatherTimeDto>> weatherRegionRequest( @RequestParam("lat") String lat,
-                                                                  @RequestParam("long") String lng) throws Exception {
-        return weatherOneRegionService.getRegionWeather(lat,lng);
+    public WeatherController(WeatherFacade weatherFacade) {
+        this.weatherFacade = weatherFacade;
     }
 
     /*
-     * 위치를 지정한 경우 주간별
+     ** 위치를 지정한 경우 시간대별
      */
-    @GetMapping("/week")
-    public ResponseEntity<List<WeatherDto>> weatherWeekRequest( @RequestParam("lat") String lat,
-                                                                @RequestParam("long") String lng) throws Exception {
-        return weatherWeekService.getWeekWeather(lat,lng);
+    @GetMapping(
+            value="/time",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<WeatherTimeApiResponse>> weatherRegionRequest(@RequestParam("lat") String lat,
+                                                                             @RequestParam("long") String lng) {
+        return ResponseEntity.ok(weatherFacade.getRegionWeather(lng, lat));
     }
 
     /*
-     * 위치를 지정하지 않은 경우
+     ** 위치를 지정한 경우 주간별
      */
-    @GetMapping("/all")
-    public ResponseEntity<ApiWeatherResult> weatherAllRequest() throws Exception {
+    @GetMapping(
+            value = "/week",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<WeatherMidResponse>> weatherWeekRequest(@RequestParam("lat") String lat,
+                                                                       @RequestParam("long") String lng) {
+        return ResponseEntity.ok(weatherFacade.getWeekWeather(lng, lat));
+    }
 
-        return weatherAllRegionService.getAllRegionWeather();
+    /*
+     ** 위치를 지정하지 않은 경우
+     */
+    @GetMapping(
+            value = "/all",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<WeatherApiResult> weatherAllRequest() {
 
+        return ResponseEntity.ok(weatherFacade.getAllRegionWeather());
     }
 
 }
