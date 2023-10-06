c08710ba1a720e62ecf3d8812959706471274632
byeolhaha <j35635aa@gmail.com>
2023-10-06 14:41:13 +0900

    [Refactor] : 관련된 책임 추가

--- src/main/java/com/everyonegarden/weather/service/TodayTimer.java
+++ src/main/java/com/everyonegarden/weather/service/TodayTimer.java
@@ -1,22 +1,18 @@
 package com.everyonegarden.weather.service;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class TodayTimer {
 
-    ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
-    ZonedDateTime seoulDateTime = ZonedDateTime.now(seoulZoneId);
-
-
-    public String getTime() {
-        LocalTime localTime = seoulDateTime.toLocalTime();
-
-        return  localTime.format(DateTimeFormatter.ofPattern("HH"));
-    }
+    private final ZoneId seoulZoneId = ZoneId.of("Asia/Seoul");
+    private final ZonedDateTime seoulDateTime = ZonedDateTime.now(seoulZoneId);
 
     public String getDay() {
         LocalDate today = seoulDateTime.toLocalDate();
@@ -28,6 +24,19 @@ public class TodayTimer {
         LocalDate tomorrow = today.plusDays(1);
 
         return tomorrow.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    public String getTmFc() {
+        String today = getDay();
+        int timeFormat = Integer.parseInt(getTime());
+
+        return today + (timeFormat >= 6 && timeFormat <= 17 ? "0600" : "1800");
+    }
+
+    public String getTime() {
+        LocalTime localTime = seoulDateTime.toLocalTime();
+
+        return localTime.format(DateTimeFormatter.ofPattern("HH"));
     }
 
 }
