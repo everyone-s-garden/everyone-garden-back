d9b36f82ef8062aaa47e971276f1e71ee0a845aa
byeolhaha <j35635aa@gmail.com>
2023-10-07 02:18:21 +0900

    [Refactor] :  전략 패턴 도입

--- src/main/java/com/everyonegarden/auth/client/ClientGoogle.java
+++ src/main/java/com/everyonegarden/auth/client/ClientGoogle.java
@@ -2,44 +2,41 @@ package com.everyonegarden.auth.client;
 
 import com.everyonegarden.auth.dto.GoogleUserResponse;
 import com.everyonegarden.auth.exception.TokenValidFailedException;
-
-
+import com.everyonegarden.global.error.ErrorCode;
 import com.everyonegarden.member.entity.Member;
+import com.everyonegarden.member.entity.RoleType;
 import com.everyonegarden.member.enunerate.MemberProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
 public class ClientGoogle implements ClientProxy{
 
     private final WebClient webClient;
+
+    public ClientGoogle(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public Member getUserData(String accessToken) {
 
         GoogleUserResponse googleUserResponse = webClient.get()
                 .uri("https://www.googleapis.com/oauth2/v3/userinfo")
-                .header("AUTHORIZATION","Bearer"+accessToken)
+                .headers(h -> h.set("Authorization",accessToken))
                 .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, response -> Mono.error(new TokenValidFailedException("Social Access Token is unauthorized")))
-                .onStatus(HttpStatus::is5xxServerError, response -> Mono.error(new TokenValidFailedException("Internal Server Error")))
+                .onStatus(status-> status.is4xxClientError(), response -> Mono.error(new TokenValidFailedException(ErrorCode.UNAUTHORIZED_TOKEN)))
+                .onStatus(status-> status.is5xxServerError(), response -> Mono.error(new TokenValidFailedException(ErrorCode.OAUTH_CLIENT_SERVER_ERROR)))
                 .bodyToMono(GoogleUserResponse.class)
                 .block();
 
-
         System.out.println("googleUserResponse:"+googleUserResponse);
-
 
         return Member.builder()
                 .socialId(googleUserResponse.getSub())
-                .name(googleUserResponse.getName())
-                .email(googleUserResponse.getEmail())
                 .memberProvider(MemberProvider.GOOGLE)
-                .roleType("ROLE_USER")
+                .roleType(RoleType.USER)
                 .build();
     }
 }
--- src/main/java/com/everyonegarden/auth/client/ClientKakao.java
+++ src/main/java/com/everyonegarden/auth/client/ClientKakao.java
@@ -1,40 +1,41 @@
 package com.everyonegarden.auth.client;
 
-
 import com.everyonegarden.auth.dto.KakaoUserResponse;
 import com.everyonegarden.auth.exception.TokenValidFailedException;
+import com.everyonegarden.global.error.ErrorCode;
 import com.everyonegarden.member.entity.Member;
+import com.everyonegarden.member.entity.RoleType;
 import com.everyonegarden.member.enunerate.MemberProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
-@RequiredArgsConstructor
 public class ClientKakao implements ClientProxy{
 
     private final WebClient webClient;
+
+    public ClientKakao(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     @Override
     public Member getUserData(String accessToken) {
 
         KakaoUserResponse kakaoUserResponse = webClient.get()
                 .uri("https://kapi.kakao.com/v2/user/me")
-                .headers(h -> h.setBearerAuth(accessToken))
+                .headers(h -> h.set("Authorization",accessToken))
                 .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, response -> Mono.error(new TokenValidFailedException("Social Access Token is unauthorized")))
-                .onStatus(HttpStatus::is5xxServerError, response -> Mono.error(new TokenValidFailedException("Internal Server Error")))
+                .onStatus(status-> status.is4xxClientError(), response -> Mono.error(new TokenValidFailedException(ErrorCode.UNAUTHORIZED_TOKEN)))
+                .onStatus(status-> status.is5xxServerError(), response -> Mono.error(new TokenValidFailedException(ErrorCode.OAUTH_CLIENT_SERVER_ERROR)))
                 .bodyToMono(KakaoUserResponse.class)
                 .block();
 
         return Member.builder()
                 .socialId(String.valueOf(kakaoUserResponse.getId()))
-                .name(kakaoUserResponse.getProperties().getNickname())
-                .email(kakaoUserResponse.getKakaoAccount().getEmail())
                 .memberProvider(MemberProvider.KAKAO)
-                .roleType("ROLE_USER")
+                .roleType(RoleType.USER)
                 .build();
     }
+
 }
