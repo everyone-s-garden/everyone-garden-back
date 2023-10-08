6c8912ddb04892c34292bc8bf65896aacbfc30dc
byeolhaha <j35635aa@gmail.com>
2023-10-08 14:21:13 +0900

    [Test] : 테스트 구현에 따른 Fixture 객체 추가

--- src/test/java/com/everyonegarden/Fixtures.java
+++ src/test/java/com/everyonegarden/Fixtures.java
@@ -1,10 +1,20 @@
 package com.everyonegarden;
 
+import com.everyonegarden.garden.garden.Garden;
+import com.everyonegarden.garden.garden.GardenStatus;
+import com.everyonegarden.garden.garden.GardenType;
+import com.everyonegarden.garden.gardenImage.GardenImage;
 import com.everyonegarden.member.entity.Member;
+import com.everyonegarden.member.entity.Name;
+import com.everyonegarden.member.entity.RoleType;
+import com.everyonegarden.member.enunerate.MemberProvider;
 import com.everyonegarden.region.entity.Region;
+import com.everyonegarden.report.entity.ReportItem;
+import com.everyonegarden.report.service.dto.ReportRegisterRequest;
 import com.everyonegarden.weather.infra.dto.*;
 import com.everyonegarden.weather.service.dto.RegionWeatherFindRequest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -85,6 +95,96 @@ public class Fixtures {
                 "PTY",
                 "0",
                 "강원"
+        );
+    }
+
+    public static ReportRegisterRequest fakeSaleReportRequest(Long postId) {
+        return new ReportRegisterRequest(
+                ReportItem.FAKED_SALE,
+                "",
+                postId
+        );
+    }
+
+    public static Garden gangwonGarden(Member member) {
+        return new Garden(
+                "강원도 춘천시 퇴계로 89",
+                37.757687,
+                128.873749,
+                "강원양떼농장",
+                GardenType.PUBLIC,
+                GardenStatus.ACTIVE,
+                "http://samplegarden.com",
+                "$10 per hour",
+                "010-1234-4567",
+                "10평",
+                "텃밭을 가꾸는 데 아주 좋은 시설을 가지고 있어요",
+                true,
+                false,
+                true,
+                member,
+                false,
+                0
+        );
+    }
+
+    public static Garden editedGangwonGarden(Member member) {
+        return new Garden(
+                "강원도 춘천시 퇴계로 89",
+                37.757687,
+                128.873749,
+                "강원양떼농장",
+                GardenType.PUBLIC,
+                GardenStatus.INACTIVE,
+                "http://samplegarden.com",
+                "$15 per hour",
+                "010-1234-4567",
+                "10평",
+                "텃밭을 가꾸는 데 아주 좋은 시설을 가지고 있어요",
+                true,
+                false,
+                true,
+                member,
+                false,
+                0
+        );
+    }
+    public static Member savedMember() {
+        return  Member.builder()
+                .id(1L)
+                .email("test@example.com")
+                .memberProvider(MemberProvider.GOOGLE)
+                .name(new Name("Kim Byeol"))
+                .socialId("1234")
+                .roleType(RoleType.USER)
+                .build();
+    }
+
+    public static Member writerMember() {
+        return  Member.builder()
+                .email("test@example.com")
+                .memberProvider(MemberProvider.GOOGLE)
+                .name(new Name("Kim Byeol"))
+                .socialId("1234")
+                .roleType(RoleType.USER)
+                .build();
+    }
+
+    public static Member reporterMember() {
+        return  Member.builder()
+                .email("test@example.com")
+                .memberProvider(MemberProvider.GOOGLE)
+                .name(new Name("Park JinGyeom"))
+                .socialId("1234")
+                .roleType(RoleType.USER)
+                .build();
+    }
+
+    public static ReportRegisterRequest reportRegisterRequest(Long postId) {
+        return new ReportRegisterRequest(
+                ReportItem.FAKED_SALE,
+                "",
+                postId
         );
     }
 
