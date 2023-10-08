a8462a2f487e8da7f6d029809dc4cda507d426e4
byeolhaha <j35635aa@gmail.com>
2023-10-08 14:18:19 +0900

    [Test] : 신고 도메인 테스트 구현

--- src/main/java/com/everyonegarden/report/entity/Report.java
+++ src/main/java/com/everyonegarden/report/entity/Report.java
@@ -2,7 +2,9 @@ package com.everyonegarden.report.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.util.Assert;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Report {
@@ -26,11 +28,14 @@ public class Report {
     private ReportItem item;
 
     public Report(Long postId, Long reporterId, String contents, ReportItem item) {
+        Assert.notNull(postId,"게시글 Id는 null이 들어올 수 없습니다.");
+        Assert.notNull(reporterId,"신고자의 Id는 null이 들어올 수 없습니다");
+        Assert.notNull(item,"신고항목은 null이 들어올 수 없습니다.");
+
         this.postId = postId;
         this.reporterId = reporterId;
         this.contents = contents;
         this.item = item;
     }
-
 
 }
